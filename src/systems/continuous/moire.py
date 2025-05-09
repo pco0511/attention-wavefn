68a425c72d837bf6d08c2ac5f73675f9411c4205
박찬우 <78363148+pco0511@users.noreply.github.com>
2025-05-09 20:42:48 +0900

    fucking laplacian fuck fuck fuck

--- src/systems/continuous/moire.py
+++ src/systems/continuous/moire.py
@@ -11,7 +11,7 @@ G = jnp.array([
     [jnp.cos(2 * jnp.pi * 2 / 3), jnp.sin(2 * jnp.pi * 2 / 3)],
 ])
 
-def potential(
+def moire_potential(
     x: Float[Array, "2"], 
     V_0: float,
     a_M: float,
@@ -40,18 +40,18 @@ def wfn_laplacian(
     wavefn: eqx.Module,
     point: Float[Array, "n_par 2"]
 ) -> Float[Array, ""]:
-    def laplacian(fn_real):
-        grad_fn = jax.grad(fn_real)
+    def laplacian(fn, x):
+        grad_fn = jax.grad(fn)
         
         def hvp(v):
-            _, hv = jax.jvp(grad_fn, (point,), (v,))
+            _, hv = jax.jvp(grad_fn, (x,), (v,))
             return hv
         
-        d = point.size # n_par * spc_dim
+        d = x.size # n_par * spc_dim
         
         def body(i, acc):
-            v_flat = jnp.zeros(d, point.dtype).at[i].set(1.)
-            v = v_flat.reshape(point.shape)
+            v_flat = jnp.zeros(d, x.dtype).at[i].set(1.)
+            v = v_flat.reshape(x.shape)
             hv = hvp(v)
             diag_i = hv.reshape(-1)[i]
             return acc + diag_i
@@ -59,10 +59,53 @@ def wfn_laplacian(
         zero = jnp.asarray(0., point.real.dtype)
         return jax.lax.fori_loop(0, d, body, zero)
     
-    lap_re = laplacian(lambda r: jnp.real(wavefn(r)))
-    lap_im = laplacian(lambda r: jnp.imag(wavefn(r)))
+    lap_re = laplacian(lambda r: jnp.real(wavefn(r)), point)
+    lap_im = laplacian(lambda r: jnp.imag(wavefn(r)), point)
 
     return jax.lax.complex(lap_re, lap_im)
+
+import time, functools
+
+
+def potential_energy(
+    point: Float[Array, "n_par 2"],
+    V_0: float,
+    a_M: float,
+    phi: float,
+) -> Float[Array, ""]:
+    potentials = jax.vmap(moire_potential, (0, None, None, None))(point, V_0, a_M, phi)
+    return jnp.sum(potentials)
+
+def interaction_energy(
+    point: Float[Array, "n_par 2"],
+    epsilon: float = 1e-6
+) -> Float[Array, ""]:
+    idx_i, idx_j = jnp.triu_indices(point.shape[0], k=1) 
+    pair_energy = jax.vmap(
+        lambda i, j: coulomb_repulsive(point[i], point[j], epsilon),
+        in_axes=(0, 0),
+        out_axes=0
+    )
+    interactions = pair_energy(idx_i, idx_j)
+    return jnp.sum(interactions)
+
+def kinetic_energy(
+    wavefn: eqx.Module,
+    point: Float[Array, "n_par 2"]
+) -> Float[Array, ""]:
+    start = time.time()
+    lap = wfn_laplacian(wavefn, point)
+    lap.block_until_ready()
+    elapsed = time.time() - start
+    print(f"    - laplacian: {elapsed:.4f} seconds")
+    
+    start = time.time()
+    psi = wavefn(point)
+    psi.block_until_ready()
+    elapsed = time.time() - start
+    print(f"    - wavefn: {elapsed:.4f} seconds")
+    
+    return (-1/2) * (lap / psi)
 
 def local_energy(
     wavefn: eqx.Module,
@@ -72,14 +115,65 @@ def local_energy(
     phi: float,
     epsilon: float = 1e-6
 ) -> Float[Array, ""]:
-    v = jnp.sum(jax.vmap(potential, (0, None, None, None))(point, V_0, a_M, phi))
-    idx_i, idx_j = jnp.triu_indices(point.shape[0], k=1) 
-    pair_energy = jax.vmap(
-        lambda i, j: coulomb_repulsive(point[i], point[j], epsilon),
-        in_axes=(0, 0),
-        out_axes=0
-    )
-    interactions = pair_energy(idx_i, idx_j)
-    u = jnp.sum(interactions)
-    t = (-1/2) * wfn_laplacian(wavefn, point) / (wavefn(point) + epsilon)
-    return jnp.real(t + v + u)
+    
+    start = time.time()
+    t = kinetic_energy(wavefn, point)
+    t.block_until_ready()
+    elapsed = time.time() - start
+    print(f"kinetic term: {elapsed:.4f} seconds")
+
+    start = time.time()
+    v = potential_energy(point, V_0, a_M, phi)
+    v.block_until_ready()
+    elapsed = time.time() - start
+    print(f"potential term: {elapsed:.4f} seconds")
+
+    start = time.time()
+    u = interaction_energy(point, epsilon)
+    u.block_until_ready()
+    elapsed = time.time() - start
+    print(f"interaction term: {elapsed:.4f} seconds")
+
+    start = time.time()
+    E_loc = t + v + u
+    E_loc.block_until_ready()
+    elapsed = time.time() - start
+    print(f"summation: {elapsed:.4f} seconds")
+
+    
+    
+    return E_loc
+
+def batched_local_energy(
+    wavefn: eqx.Module,
+    points: Float[Array, "batch n_par 2"],
+    V_0: float,
+    a_M: float,
+    phi: float,
+    epsilon: float = 1e-6
+) -> Float[Array, "batch"]:
+    # with jax.profiler.TraceAnnotation("kienetic"):
+    start = time.time()
+    t = jax.vmap(kinetic_energy, (None, 0))(wavefn, points)
+    t.block_until_ready()
+    elapsed = time.time() - start
+    print(f"kinetic term: {elapsed:.4f} seconds")
+    # with jax.profiler.TraceAnnotation("potential"):
+    start = time.time()
+    v = jax.vmap(potential_energy, (0, None, None, None))(points, V_0, a_M, phi)
+    v.block_until_ready()
+    elapsed = time.time() - start
+    print(f"potential term: {elapsed:.4f} seconds")
+    # with jax.profiler.TraceAnnotation("interaction"):
+    start = time.time()
+    u = jax.vmap(interaction_energy, (0, None))(points, epsilon)
+    u.block_until_ready()
+    elapsed = time.time() - start
+    print(f"interaction term: {elapsed:.4f} seconds")
+    # with jax.profiler.TraceAnnotation("sum"):
+    start = time.time()
+    E_locs = t + v + u
+    E_locs.block_until_ready()
+    elapsed = time.time() - start
+    print(f"sum: {elapsed:.4f} seconds")
+    return E_locs
